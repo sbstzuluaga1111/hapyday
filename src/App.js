68d62223e1bf62c329b6b2416550722a7c0b27b0
sbstzuluaga1111 <sbstzuluagajob@gmail.com>
2024-05-12 13:47:46 -0500

    detalle

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="overlay">
+        <h1>Feliz Día de la Madre</h1>
+        <h2>Madre: Faro de Amor en la Noche de la Vida</h2>
+        <div className="texto">
+          <p>
+            En el vaivén de la vida, un faro de amor emerge,
+            Madre, sostén firme, luz que en la oscuridad surge.
+            En tu ser anida la entrega sin medida,
+            Eres brújula, ancla, en esta vida tan recorrida.
+            <br></br>
+            <br></br>
+            Con cada amanecer, labras un camino de fe,
+            Tejes sueños en el alba, con destreza y con fe.
+            En las noches oscuras, eres guía y consuelo,
+            Tu amor, un refugio en el vasto desvelo.
+            <br></br>
+            <br></br>
+            Porque en tu ser anida la entrega sin medida,
+            Eres brújula, ancla, en esta vida tan recorrida.
+            Que cada rayo de sol sea un abrazo en tu piel,
+            Que cada estrella en el cielo sea un deseo fiel.
+            <br></br>
+            <br></br>
+            Que en este día de las madres, en tu honor,
+            Brille el amor que siempre emanas con fervor.
+            Madre, pilar de fortaleza y de nobleza,
+            Te deseamos un día lleno de dicha y belleza.
+          </p> 
+        </div>
+      </div>
       </header>
-    </div>
+    </div> 
   );
 }
 
